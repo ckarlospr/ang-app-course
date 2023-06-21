44fb24b03f816276254f07674bf207661e28e4ca
Carlos Perez <per.ckarlos@gmail.com>
2023-06-20 20:01:56 -0500

    praticing binding

--- src/app/persona/persona.component.ts
+++ src/app/persona/persona.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./persona.component.css']
 })
 export class PersonaComponent {
+  nombre: string = 'Juan';
+  apellido: string = 'Perez';
+  edad: number = 28;
+  
+  /* se puede hacer privado haciendo uso de los getters y setters
+  private edad: number = 28;*/
 
+  /*getEdad():number{
+    return this.edad;
+  }*/
 }
